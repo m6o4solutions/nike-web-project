d09ed163552251a70618dff5921defe87f93931c
Michael Owuor <michael.owuor@gmail.com>
2023-09-08 20:23:44 +0300

    feat: hero section and shoe cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,20 +1,24 @@
 import CustomerReviews from './customer-reviews';
+import Button from './ui/button';
 import Footer from './footer';
 import Hero from './hero';
 import NavigationBar from './navigation-bar';
 import PopularProducts from './popular-products';
 import Services from './services';
+import ShoeCard from './ui/shoe-card';
 import SpecialOffer from './special-offer';
 import Subscribe from './subscribe';
 import SuperQuality from './super-quality';
 
 export {
 	CustomerReviews,
+	Button,
 	Footer,
 	Hero,
 	NavigationBar,
 	PopularProducts,
 	Services,
+	ShoeCard,
 	SpecialOffer,
 	Subscribe,
 	SuperQuality,
